a702b5024dff1dffdcef1c9fc4e23e6fc2cbe846
pronay7777 <pronayyanorp@gmail.com>
2026-08-21 09:55:23 +0530

    Rename file to 'Session 6 Task 1 VTU30566.java'

--- WEEK 2/Session 6 Task 1 VTU30566.java
+++ WEEK 2/Session 6 Task 1 VTU30566.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             nums[i] = nums[i] + n * (nums[nums[i]] % n);
         }
-
+    
         for (int i = 0; i < n; i++) {
             nums[i] = nums[i] / n;
         }
